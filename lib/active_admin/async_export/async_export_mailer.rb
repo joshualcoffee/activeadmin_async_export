86ab43fa9044a90a759128f6dfa5b680cd17eb87
Joshua Coffee <josh@joffeedesign.com>
2014-05-19 13:03:20 -0400

    adding params with async

--- lib/active_admin/async_export/async_export_mailer.rb
+++ lib/active_admin/async_export/async_export_mailer.rb
@@ -3,13 +3,14 @@ module ActiveAdmin
     class AsyncExportMailer < ActionMailer::Base
       add_template_helper MethodOrProcHelper
 
-      def csv_export(admin_email, model_name , collection)
+      def csv_export(admin_email, model_name, params )
         controller = Kernel::qualified_const_get("Admin::#{model_name}sController").new
         config = controller.send(:active_admin_config)
         path = controller.send(:active_admin_template, 'index.csv')
         csv_filename = controller.send(:csv_filename)
-        
-        app = ActiveAdmin.application
+
+        app = ActiveAdmin.application 
+        collection = csv_collection(model_name,params)
 
         csv = render_to_string(file: path,
                          layout: false,
@@ -25,6 +26,25 @@ module ActiveAdmin
              body: 'See attached',
              from: ActiveAdmin::AsyncExport.from_email_address)
       end
+
+      def csv_collection(model,params)
+        klass = model.constantize
+        @search = klass.search  clean_search_params params['q']
+
+        @search.to_a
+      end
+
+      def clean_search_params(search_params)
+        return {} unless search_params.is_a?(Hash)
+        
+
+        search_params = search_params.dup
+        search_params.delete_if do |key, value|
+          value == ""
+        end
+        search_params
+      end
+
     end
   end
 end
